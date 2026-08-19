b1f369839d377cf35e872719bf6e714150c77530
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-08-19 22:13:00 +0400

    Refactor: putting code block which inserts the values to the products table to try/catch block

--- ProductManager.cs
+++ ProductManager.cs
@@ -6,22 +6,30 @@ namespace InvoicingApp
     {
         public async Task AddProducts(Product p)
         {
-            var (id, productName,  price) = p;
-
-            string sql = @"INSERT INTO products (id,productName,price) 
-            VALUES(@id,@productName,@price)";
-            Console.WriteLine(id);
-            Console.WriteLine(productName);
-            Console.WriteLine(price);
-            if (dataSource != null && id != null && productName != null  && price != null)
+            try
             {
-                Console.WriteLine("i am here");
-                await using var cmd = dataSource.CreateCommand(sql);
-                cmd.Parameters.AddWithValue("id", id);
-                cmd.Parameters.AddWithValue("productName", productName);
-                cmd.Parameters.AddWithValue("price", price);
-                await cmd.ExecuteNonQueryAsync();
+                var (id, productName, price) = p;
+
+                string sql = @"INSERT INTO products (id,productName,price) 
+            VALUES(@id,@productName,@price)";
+                Console.WriteLine(id);
+                Console.WriteLine(productName);
+                Console.WriteLine(price);
+                if (dataSource != null && id != null && productName != null && price != null)
+                {
+                    Console.WriteLine("i am here");
+                    await using var cmd = dataSource.CreateCommand(sql);
+                    cmd.Parameters.AddWithValue("id", id);
+                    cmd.Parameters.AddWithValue("productName", productName);
+                    cmd.Parameters.AddWithValue("price", price);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
+        
     }
 }
