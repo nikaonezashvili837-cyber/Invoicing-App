0ab2624fa3c15b30d305c56b0365be5002f7bc0f
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-08-25 22:40:30 +0400

    Feat:building Invoice generator menu

---
+++ Invoice.cs
@@ -0,0 +1,8 @@
+namespace InvoicingApp
+{
+    class Invoice
+    {
+       public string? Customer {get;set;}
+       public List<Product>? SelectedProducts {get;set;}
+    }
+}
---
+++ InvoiceGenerator.cs
@@ -0,0 +1,50 @@
+namespace InvoicingApp
+{
+    public partial class Program
+    {
+        public static void InvoiceGenerator()
+        {
+            Console.WriteLine("--- Create Invoice ---");
+
+            Console.Write("Customer Id: ");
+            string customerId = Console.ReadLine()!;
+
+
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("--- Add Line Item ---");
+
+                Console.Write("Item id: ");
+                string itemId = Console.ReadLine()!;
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Add another item? (Y/N): ");
+                string? answer = Console.ReadLine();
+
+                if (answer?.ToUpper() != "Y")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Discount (%): ");
+            decimal discountPercent = decimal.Parse(Console.ReadLine()!);
+
+            Console.Write("[1] Standard VAT (1%)  [2] Zero-rated : ");
+            int vatType = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Due date (yyyy-MM-dd): ");
+            DateTime dueDate = DateTime.Parse(Console.ReadLine()!);
+
+            Console.WriteLine();
+            Console.WriteLine("--- Invoice Data Collected ---");
+
+        }
+    }
+}
