8f25ac026c33288da8c40572085924c700a44b91
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-08-27 22:01:35 +0400

    Modifiying SelectedProducts array

--- Invoice.cs
+++ Invoice.cs
@@ -1,8 +1,22 @@
+using System.Security.Cryptography;
+
 namespace InvoicingApp
 {
+    struct SelectedProductData
+    {
+        public Product SelectedProduct { get; }
+        public int Amount { get; }
+        public SelectedProductData(Product SelectedProduct, int Amount)
+        {
+            this.SelectedProduct = SelectedProduct;
+            this.Amount = Amount;
+        }
+    }
     class Invoice
     {
-       public string? Customer {get;set;}
-       public List<Product>? SelectedProducts {get;set;}
+
+
+        public string? Customer { get; set; }
+        public List<SelectedProductData>? SelectedProducts { get; set; }
     }
 }
